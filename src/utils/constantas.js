faed689e36e77e9809e92c20dbf717ac1294635e
Artem Bulychev <twist1996@icloud.com>
2023-09-17 22:59:50 +0500

    deleting all content from branch

--- src/utils/constantas.js
+++
@@ -1,4 +0,0 @@
-export const routesWithHeader = ["/", "/movies", "/saved-movies", "/profile"];
-export const routesWithoutBlueHeader =["/movies", "/saved-movies", "/profile"];
-export const routesWithFooter = ["/", "/movies", "/saved-movies"];
-export const SCREEN_MD = 768;
